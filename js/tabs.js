7d8c13cd164938ec248a148bdeab93c458fe0772
Velizar Shulev <1103834s@bo720-10-02.(none)>
2013-11-26 20:39:18 +0000

    Added initial files

---
+++ js/tabs.js
@@ -0,0 +1,12 @@
+
+function Tabs(elementID, callback) {
+
+	this.name = new Array();
+	this.tabsContainer = document.getElementById(elementID);
+
+	var children = this.tabsContainer.childNodes;
+	for(i = 0; i < children.length; i++) {	
+		children[i].onclick = callback;
+	}
+
+}
